addb65f7474ffe82d96e478a8f4fd95e3649ff85
fuchunfen <fuchunfen09@sina.com>
2017-02-27 08:59:14 +0800

    add UIBestPractice exercise code

---
+++ UIBestPractice/app/src/main/java/com/fanny/uibestpractice/Msg.java
@@ -0,0 +1,28 @@
+package com.fanny.uibestpractice;
+
+/**
+ * Created by Administrator on 2017/2/26 0026.
+ */
+public class Msg {
+
+    public static final int TYPE_RECEIVED = 0;
+
+    public static final int TYPE_SENT = 1;
+
+    private String content;
+
+    private int type;
+
+    public Msg(String content, int type){
+        this.content = content;
+        this.type = type;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+    public int getType(){
+        return type;
+    }
+}
