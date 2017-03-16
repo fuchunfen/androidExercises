5481ddc98f3fe657a98b9c08185fa82c7c79f131
fuchunfen <fuchunfen09@sina.com>
2017-03-16 17:47:57 +0800

    加入创建数据库和表的各项配置

---
+++ CoolWeather/app/src/main/java/com/coolweather/android/db/Provice.java
@@ -0,0 +1,38 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/3/16 0016.
+ */
+
+public class Provice extends DataSupport {
+
+    private int id;
+    private String proviceName;
+    private int proviceCode;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProviceName(String proviceName) {
+        this.proviceName = proviceName;
+    }
+
+    public void setProviceCode(int proviceCode) {
+        this.proviceCode = proviceCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProviceName() {
+        return proviceName;
+    }
+
+    public int getProviceCode() {
+        return proviceCode;
+    }
+}
